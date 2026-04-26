4bf30e81791beaa4b4fba77d8d882f3d18c69406
tehleader1 <xxfigueroa1993@yahoo.com>
2026-04-26 03:38:56 -0400

    Initialize functional surfaces and voice assistants

--- static/app.v20260320h.js
+++ static/app.v20260320h.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   if (window.SupportRDRebuild?.initCommerceRank) {
     window.SupportRDRebuild.initCommerceRank();
+    window.SupportRDRebuild.initFunctionalSurfaces?.();
+    window.SupportRDRebuild.initVoiceAssistants?.();
   }
 });
